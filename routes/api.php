a056c5a5e2740fd6b07922bb29b7591f0f30c9df
Kayo Bruno <kayodw@gmail.com>
2021-07-08 20:48:26 -0300

    Adicionar middleware para criação de logs
    Os logs serão gerados todas as vezes em que um endpoint da gestão de
    alertas for chamado. Os logs estão no formato "json_inline" e serão
    armazenados no diretório padrão de logs do framework.

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('alerts', 'Api\AlertController');
+Route::resource('alerts', 'Api\AlertController')->middleware('log.route');
